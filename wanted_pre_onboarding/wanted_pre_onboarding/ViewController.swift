f302c29d69016c57fb5fb38615fb26f88c4ecfb8
hhhan0315 <hhan0315@naver.com>
2022-06-09 19:31:29 +0900

    [Feat] 프로젝트 초기 생성 및 Main.storyboard 삭제

---
+++ wanted_pre_onboarding/wanted_pre_onboarding/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  wanted_pre_onboarding
+//
+//  Created by rae on 2022/06/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
