673d8a10709642520db4be20aa23be4609ed3ac6
hhhan0315 <hhan0315@naver.com>
2022-06-12 21:02:34 +0900

    [Feat] HomeViewModel 구현

---
+++ wanted_pre_onboarding/wanted_pre_onboarding/Presentation/HomeViewModel.swift
@@ -0,0 +1,33 @@
+//
+//  HomeViewModel.swift
+//  wanted_pre_onboarding
+//
+//  Created by rae on 2022/06/10.
+//
+
+import Foundation
+
+class HomeViewModel {
+    private(set) var weatherResponses: Observable<[WeatherResponse]>
+    private let cityNames = ["Gongju", "Gwangju", "Gumi", "Gunsan", "Daegu", "Daejeon", "Mokpo", "Busan", "Seosan", "Seoul", "Sokcho", "Suwon", "Suncheon", "Ulsan", "Iksan", "Jeonju", "Jeju", "Cheonan", "Cheongju-si", "Chuncheon"]
+    private let networkService: NetworkService
+    
+    init() {
+        self.weatherResponses = Observable([])
+        self.networkService = NetworkService()
+    }
+    
+    func fetch() {
+        cityNames.forEach { cityName in
+            let request = WeatherRequest(cityName: cityName)
+            self.networkService.request(request) { (result: Result<WeatherResponse, NetworkError>) in
+                switch result {
+                case .success(let weatherResponse):
+                    self.weatherResponses.value.append(weatherResponse)
+                case .failure(let error):
+                    print(error)
+                }
+            }
+        }
+    }
+}
